6a48446f760c0251a25a3801ae0dca7b924ce06a
AbhayWankhade001 <abhaywankhade2004@gmail.com>
2023-03-11 20:41:27 +0530

    this is backend hope it will work(its goin to work now it's goin to wor)

---
+++ database/conn.js
@@ -0,0 +1,53 @@
+// import mongoose from "mongoose";
+
+// import { MongoMemoryServer } from "mongodb-memory-server";
+// import ENV from '../router/config.js'
+
+// async function connect(){
+
+//     const mongod = await MongoMemoryServer.create();
+//     const getUri = mongod.getUri();
+
+//     mongoose.set('strictQuery', true)
+//     // const db = await mongoose.connect(getUri);
+//     const db = await mongoose.connect(ENV.ATLAS_URI);
+//     console.log("Database Connected")
+//     return db;
+// }
+
+// export default connect;
+// import mongoose from "mongoose";
+// import ENV from '../router/config.js';
+
+// async function connect(){
+//     const db = await mongoose.connect(ENV.ATLAS_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     });
+//     console.log("Database Connected");
+//     return db;
+// }
+
+// export default connect;
+
+
+import mongoose from "mongoose";
+import ENV from '../router/config.js';
+
+async function connect(){
+    try {
+        const db = await mongoose.connect(ENV.ATLAS_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+           
+            serverSelectionTimeoutMS: 5000 // Add this option
+        });
+        console.log("Database Connected");
+        return db;
+    } catch (error) {
+        console.error("Error connecting to database:", error.message);
+        process.exit(1); // Exit the process with an error code
+    }
+}
+
+export default connect;
